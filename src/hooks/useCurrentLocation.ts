e62667944799f96a38e0a230e274cb368f2a8ddf
David Piper <dbpiper@gmail.com>
2024-06-11 12:12:32 -0500

    Fix lint issue with scope in useCurrentLocation

--- src/hooks/useCurrentLocation.ts
+++ src/hooks/useCurrentLocation.ts
@@ -2,34 +2,34 @@ import { useState, useEffect } from 'react';
 import { PermissionsAndroid, Platform } from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
 
+const requestLocationPermission = async () => {
+    if (Platform.OS === 'android') {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                {
+                    title: 'Geolocation Permission',
+                    message: 'Can we access your location?',
+                    buttonNeutral: 'Ask Me Later',
+                    buttonNegative: 'Cancel',
+                    buttonPositive: 'OK',
+                }
+            );
+            return granted === PermissionsAndroid.RESULTS.GRANTED;
+        } catch (error_) {
+            console.warn(error_);
+            return false;
+        }
+    }
+    return true; // iOS permissions are handled in the Info.plist file
+};
+
 export const useCurrentLocation = () => {
     const [location, setLocation] = useState<{
         latitude: number;
         longitude: number;
     }>({ latitude: 0, longitude: 0 });
     const [error, setError] = useState<string | undefined>();
-
-    const requestLocationPermission = async () => {
-        if (Platform.OS === 'android') {
-            try {
-                const granted = await PermissionsAndroid.request(
-                    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-                    {
-                        title: 'Geolocation Permission',
-                        message: 'Can we access your location?',
-                        buttonNeutral: 'Ask Me Later',
-                        buttonNegative: 'Cancel',
-                        buttonPositive: 'OK',
-                    }
-                );
-                return granted === PermissionsAndroid.RESULTS.GRANTED;
-            } catch (error_) {
-                console.warn(error_);
-                return false;
-            }
-        }
-        return true; // iOS permissions are handled in the Info.plist file
-    };
 
     const getCurrentLocation = async () => {
         const hasPermission = await requestLocationPermission();

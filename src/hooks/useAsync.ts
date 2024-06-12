6726d6156de05ea98486ca80da0d6a27386f17ee
David Piper <dbpiper@gmail.com>
2024-06-12 16:42:41 -0500

    Fix types

--- src/hooks/useAsync.ts
+++ src/hooks/useAsync.ts
@@ -1,6 +1,7 @@
 import { DependencyList, useEffect, useState, useCallback } from 'react';
 
-type FunctionReturningPromise = <T>(...args: T[]) => Promise<T>;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type FunctionReturningPromise<T> = (...args: any[]) => Promise<T>;
 
 type AsyncState<T> =
     | { loading: boolean; error?: undefined; value?: undefined }
@@ -8,26 +9,37 @@ type AsyncState<T> =
     | { loading: false; error: Error; value?: undefined }
     | { loading: false; error?: undefined; value: T };
 
-type StateFromFunctionReturningPromise<T extends FunctionReturningPromise> =
-    AsyncState<ReturnType<T> extends Promise<infer R> ? R : never>;
+type StateFromFunctionReturningPromise<
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    T extends FunctionReturningPromise<any>,
+> = AsyncState<ReturnType<T> extends Promise<infer R> ? R : never>;
 
-export function useAsync<T extends FunctionReturningPromise>(
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function useAsync<T extends FunctionReturningPromise<any>>(
     fn: T,
     deps: DependencyList = []
 ): StateFromFunctionReturningPromise<T> {
     const [state, setState] = useState<StateFromFunctionReturningPromise<T>>({
         loading: true,
-    });
+    } as StateFromFunctionReturningPromise<T>);
 
     const callback = useCallback(() => {
         setState((prevState) => ({ ...prevState, loading: true }));
 
         // eslint-disable-next-line promise/catch-or-return
         fn().then(
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-            (value) => setState({ value, loading: false }),
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-            (error) => setState({ error, loading: false })
+            (value) =>
+                setState({
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                    value,
+                    loading: false,
+                } as StateFromFunctionReturningPromise<T>),
+            (error) =>
+                setState({
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                    error,
+                    loading: false,
+                } as StateFromFunctionReturningPromise<T>)
         );
     }, deps);
 

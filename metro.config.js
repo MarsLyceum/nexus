af0bad5cecb71fe0f229a934e4c35fd370b2bf6b
David Piper <dbpiper@gmail.com>
2024-06-21 17:19:32 -0500

    Stub assert

--- metro.config.js
+++ metro.config.js
@@ -14,6 +14,7 @@ const config = {
             crypto: require.resolve('crypto-browserify'),
             url: require.resolve('url'),
             events: require.resolve('events'),
+            assert: require.resolve('assert'),
         },
     },
     transformer: {

f88c096e2102327b2efb5bb52ff1ccd6024b5718
David Piper <dbpiper@gmail.com>
2025-02-04 09:11:08 -0600

    Add feed channel to type

--- src/types/Group.ts
+++ src/types/Group.ts
@@ -20,7 +20,7 @@ export type GroupChannelMessage = {
 export type GroupChannel = {
     id: string;
     name: string;
-    type: 'text' | 'voice';
+    type: 'text' | 'voice' | 'feed';
     createdAt: Date;
     groupId: string;
     messages: GroupChannelMessage[];

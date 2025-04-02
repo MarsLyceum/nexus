62b5da785962b4a701410316f52d19be686c5b0e
David Piper <dbpiper@gmail.com>
2025-04-02 10:21:35 -0500

    Silence expo polyfill

--- apps/web/polyfills/expo-polyfills.js
+++ apps/web/polyfills/expo-polyfills.js
@@ -1,8 +1,3 @@
 import { EventEmitter } from 'expo-modules-core';
 
-console.log(
-    'Expo polyfills injected:',
-    globalThis.expo,
-    'EventEmitter',
-    EventEmitter
-);
+export const expoPolyfillLoaded = Boolean(EventEmitter);

7e663a2649432c58aca026f6120e29d90ddd43cc
David Piper <dbpiper@gmail.com>
2025-05-08 14:17:03 -0500

    Add gif player to mobile image renderer

--- packages/shared-ui/src/small-components/image-modal/MobileImageRenderer.tsx
+++ packages/shared-ui/src/small-components/image-modal/MobileImageRenderer.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useMemo, useState } from 'react';
 import { View, useWindowDimensions } from 'react-native';
 import {
     ResumableZoom,
@@ -6,6 +6,9 @@ import {
     useImageResolution,
 } from 'react-native-zoom-toolkit';
 
+import { useGifPlayer } from '../../hooks';
+import { GifPlayer } from '../GifPlayer';
+import { GifPlayerControls } from '../GifPlayerControls';
 import { NexusImage } from '../NexusImage';
 
 export type MobileImageRendererProps = {
@@ -20,6 +23,43 @@ export const MobileImageRenderer: React.FC<MobileImageRendererProps> = ({
     const { width: screenWidth, height: screenHeight } = useWindowDimensions();
     const { isFetching, resolution } = useImageResolution({ uri });
     const zoomRef = useRef<ResumableZoomType>(null);
+    const [controlsRect, setControlsRect] = useState<
+        | {
+              left: number;
+              top: number;
+              right: number;
+              bottom: number;
+          }
+        | undefined
+    >();
+    const controlsRef = useRef<View | undefined>();
+
+    const isGif = useMemo(() => uri.toLowerCase().endsWith('.gif'), [uri]);
+    const {
+        position,
+        playing,
+        totalDuration,
+        togglePlay,
+        onSlidingStart,
+        onValueChange,
+        onSlidingComplete,
+    } = useGifPlayer(uri);
+
+    const handleLayoutControls = useCallback(() => {
+        if (
+            controlsRef.current &&
+            typeof controlsRef.current.measure === 'function'
+        ) {
+            controlsRef.current.measure((fx, fy, width, height, px, py) => {
+                setControlsRect({
+                    left: px,
+                    top: py,
+                    right: px + width,
+                    bottom: py + height,
+                });
+            });
+        }
+    }, []);
 
     // Ensure image is ready to be rendered.
     const ready =
@@ -58,8 +98,21 @@ export const MobileImageRenderer: React.FC<MobileImageRendererProps> = ({
                     localY < rect.y ||
                     localY > rect.y + rect.height
                 ) {
-                    e.stopPropagation?.();
-                    onClose();
+                    if (
+                        isGif &&
+                        controlsRect &&
+                        (localX < controlsRect.left ||
+                            localX > controlsRect.right ||
+                            localY < controlsRect.top ||
+                            localY > controlsRect.bottom)
+                    ) {
+                        e.stopPropagation?.();
+                        onClose();
+                    }
+                    if (!isGif) {
+                        e.stopPropagation?.();
+                        onClose();
+                    }
                 }
             } else if (
                 absoluteY < offsetY ||
@@ -69,7 +122,7 @@ export const MobileImageRenderer: React.FC<MobileImageRendererProps> = ({
                 onClose();
             }
         },
-        [onClose, offsetY, imageHeight]
+        [onClose, offsetY, imageHeight, controlsRect, isGif]
     );
 
     return (
@@ -97,17 +150,40 @@ export const MobileImageRenderer: React.FC<MobileImageRendererProps> = ({
                     }}
                     tapsEnabled
                 >
-                    <NexusImage
-                        source={uri}
-                        style={{ width: imageWidth, height: imageHeight }}
-                        contentFit="contain"
-                        width={imageWidth}
-                        height={imageHeight}
-                        alt="Mobile image preview"
-                    />
+                    {isGif ? (
+                        <GifPlayer
+                            source={uri}
+                            width={imageWidth}
+                            height={imageHeight}
+                            position={position}
+                        />
+                    ) : (
+                        <NexusImage
+                            source={uri}
+                            style={{ width: imageWidth, height: imageHeight }}
+                            contentFit="contain"
+                            width={imageWidth}
+                            height={imageHeight}
+                            alt="Mobile image preview"
+                        />
+                    )}
                 </ResumableZoom>
             ) : (
                 <View style={{ flex: 1 }} />
+            )}
+            {isGif && (
+                <GifPlayerControls
+                    playing={playing}
+                    position={position}
+                    totalDuration={totalDuration}
+                    onTogglePlay={togglePlay}
+                    onSlidingStart={onSlidingStart}
+                    onValueChange={onValueChange}
+                    onSlidingComplete={onSlidingComplete}
+                    // @ts-expect-error ref
+                    ref={controlsRef}
+                    onLayout={handleLayoutControls}
+                />
             )}
         </View>
     );
